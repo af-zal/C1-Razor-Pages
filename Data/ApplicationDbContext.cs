eff1114b16ea06330e4072a509dd872d6e0a870b
Afzal <130027973+af-zal@users.noreply.github.com>
2023-04-17 18:31:42 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using S4_RazorPages.Models;
+
+namespace S4_RazorPages.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<PizzaOrder> PizzaOrders { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
